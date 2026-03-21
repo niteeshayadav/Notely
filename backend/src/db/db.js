ed6a05c93a4771734aebceede32a99339d288dd2
Niteesha Yadav Panchadarla <niteeshayadav66@gmail.com>
2026-03-21 13:54:48 +0530

    Encoded Mongo URI for security using .env

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
 
 async function connectDB() {
     try{
-        await mongoose.connect("mongodb+srv://niteeshayadav:3bSR2zPiiiEgmjga@notes-cluster.lopau6u.mongodb.net/notes_db");
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("connected to DB");
     } catch (error) {
         console.error("Error connecting to MongoDB:", error);
